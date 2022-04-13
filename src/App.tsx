20b75f3f60b19754adabcfb11d72787b79546dcd
WILewis <79179787+WILewis@users.noreply.github.com>
2022-04-12 20:43:11 -0400

    updated welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,10 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">UD CISC Degree Planner</header>
             <p>
-                Welcome to the UD CISC Degree Planner! This application is for creating potential course outlines for
-                UD CISC Majors. Start by creating a plan below, and then begin filling in the semesters with the appropriate courses.
+                Welcome to the UD CISC Degree Planner! This application is for
+                creating potential course outlines for UD CISC Majors. Start by
+                creating a plan below, and then begin filling in the semesters
+                with the appropriate courses.
             </p>
             <p>Isaac Lewis</p>
             <p>Alexander Trunzo</p>
