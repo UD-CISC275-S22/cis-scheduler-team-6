222ffe9e663b636c5a0e1e8b3867026d58a0b12b
Alexander Trunzo <adtrunzo@udel.edu>
2022-04-19 15:28:27 -0400

    Merge branch 'main' of https://github.com/UD-CISC275-S22/cis-scheduler-team-6 into adtrunzo-add-name

--- src/courseDropdown.tsx
+++ src/courseDropdown.tsx
@@ -47,8 +47,105 @@ export function CoursesSelect({
                     ))}
                 </Form.Select>
             </Form.Group>
-            {visible && userSelection + userSelection2}
-            <Button onClick={() => setVisible(!visible)}>Add Course</Button>
+            <hr></hr>
+            <EditCourse></EditCourse>
         </div>
     );
+
+    function EditCourse(): JSX.Element {
+        const [course, setCourse] = useState({
+            id: +userSelection2,
+            name: userSelection,
+            credits: 3,
+            req: "",
+            offered: ""
+        });
+        const [editState, setEditState] = useState<boolean>(false);
+
+        function updateEditState(event: React.ChangeEvent<HTMLInputElement>) {
+            setEditState(event.target.checked);
+        }
+        function updateCourseID(event: React.ChangeEvent<HTMLInputElement>) {
+            setCourse({ ...course, id: event.target.valueAsNumber });
+        }
+        function updateCourseName(event: React.ChangeEvent<HTMLInputElement>) {
+            setCourse({ ...course, name: event.target.value });
+        }
+        function updateCourseCredits(
+            event: React.ChangeEvent<HTMLInputElement>
+        ) {
+            setCourse({ ...course, credits: event.target.valueAsNumber });
+        }
+        function updateCourseReq(event: React.ChangeEvent<HTMLInputElement>) {
+            setCourse({ ...course, req: event.target.value });
+        }
+        function updateCourseOffered(
+            event: React.ChangeEvent<HTMLInputElement>
+        ) {
+            setCourse({ ...course, offered: event.target.value });
+        }
+        function setDefault() {
+            setCourse({ ...course, name: userSelection });
+            setCourse({ ...course, id: +userSelection2 });
+            setCourse({ ...course, credits: 3 });
+            setCourse({ ...course, req: "" });
+            setCourse({ ...course, offered: "" });
+        }
+        return (
+            <div>
+                <div>
+                    <Form.Check
+                        type="switch"
+                        id="check-edit"
+                        label="Edit?"
+                        checked={editState}
+                        onChange={updateEditState}
+                    />
+                </div>
+                <div>
+                    {editState && (
+                        <Form.Group controlId="changeTextBox">
+                            <Form.Label>Change Name:</Form.Label>
+                            <Form.Control
+                                type="string"
+                                value={course.name}
+                                onChange={updateCourseName}
+                            />
+                            <Form.Label>Change ID:</Form.Label>
+                            <Form.Control
+                                type="number"
+                                value={course.id}
+                                onChange={updateCourseID}
+                            />
+                            <Form.Label>Change Credits:</Form.Label>
+                            <Form.Control
+                                type="number"
+                                value={course.credits}
+                                onChange={updateCourseCredits}
+                            />
+                            <Form.Label>Change PreRequisites:</Form.Label>
+                            <Form.Control
+                                type="string"
+                                value={course.req}
+                                onChange={updateCourseReq}
+                            />
+                            <Form.Label>Change Offered:</Form.Label>
+                            <Form.Control
+                                type="string"
+                                value={course.offered}
+                                onChange={updateCourseOffered}
+                            />
+                            <Button onClick={() => setDefault()}>
+                                Set to Default
+                            </Button>
+                        </Form.Group>
+                    )}
+                </div>
+                {visible}
+                <Button onClick={() => setVisible(!visible)}>
+                    Add {course.name + course.id}
+                </Button>
+            </div>
+        );
+    }
 }
